0144669a1503974d2e88d792aec9be75fa07194a
Raghav <raghav@Raghavs-MacBook-Pro.local>
2017-04-10 14:13:23 +0530

    test commit

--- Test Suites/TestSuite1.ts
+++ Test Suites/TestSuite1.ts
@@ -4,17 +4,11 @@
    <name>TestSuite1</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2017-04-10T10:41:37</lastRun>
+   <lastRun>2017-04-10T14:08:29</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>5</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <testSuiteGuid>a40d7008-e9ea-4427-b870-ba87cc797848</testSuiteGuid>
-   <testCaseLink>
-      <guid>8e7669c6-5d53-4f0c-8b6d-d837f7e645a9</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Test 1 by Tom</testCaseId>
-   </testCaseLink>
+   <testSuiteGuid>7449c144-14b2-4e41-af79-175e08691357</testSuiteGuid>
 </TestSuiteEntity>
