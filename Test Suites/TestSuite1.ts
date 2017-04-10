4b80be9329875fdb6ab9fc821deaf71fdabe989b
Raghav <raghav@Raghavs-MacBook-Pro.local>
2017-04-10 11:19:32 +0530

    test

--- Test Suites/TestSuite1.ts
+++ Test Suites/TestSuite1.ts
@@ -4,7 +4,7 @@
    <name>TestSuite1</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2017-04-10T11:11:32</lastRun>
+   <lastRun>2017-04-10T11:18:36</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>5</pageLoadTimeout>
