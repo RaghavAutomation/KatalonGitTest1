6226ebf61b910a42b11e2e50c32ed4f2314eef62
Raghav <raghav@Raghavs-MacBook-Pro.local>
2017-04-10 11:22:35 +0530

    test

--- Scripts/Test 2 by Kim/Script1491785119566.groovy
+++ Scripts/Test 2 by Kim/Script1491785119566.groovy
@@ -19,3 +19,7 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKe
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
+WebUI.openBrowser('https://www.katalon.com')
+
+WebUI.closeBrowser()
+
